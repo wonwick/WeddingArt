4f2407c32139550eca9dc9e9d750995fa4e803ac
amiladarshana1994 <31099677+amiladarshana1994@users.noreply.github.com>
2017-10-23 13:55:25 +0530

    page_about

--- application/views/about.php
+++ application/views/about.php
@@ -22,37 +22,49 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 			<div class="about-box">
 			 <div class="cont1 span_2_of_a about_desc">
 				       <h2>About Us</h2>
-		    			   <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit,incididunt ut labore et dolore magna aliqua. </span></p>
-				           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-				            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+		    			   <p><span></span></p>
+				           <p>Before two hearts unite in celebration, a lot goes into preparations. Before the love is declared in public, there are many defining moments of choosing and picking. We at Siritha Wedding Planner will create the dream wedding you have always wished for, without a single detail missing.</p>
+				            <p>It’s not easy turning your dream wedding into a reality. With today’s hectic lifestyle, many couples and their families do not have time for the detailed planning and work a beautiful wedding of their dream.</p>
+					        <p>We at Siritha Wedding Planner, as the pioneers in Sri Lanka wedding planning work together with you at any stage of the planning process to ensure that your wedding is planned to a perfect, totally enjoyable, stress free and memorable one. After all, it is your wedding. We are there to help you – and to make your day perfect.We at Siritha Wedding Planner, as the pioneers in Sri Lanka wedding planning work together with you at any stage of the planning process to ensure that your wedding is planned to a perfect, totally enjoyable, stress free and memorable one. After all, it is your wedding. We are there to help you – and to make your day perfect.</p>
+					        <p>Our counseling alone would provide you the crème de la crème of the industry specialists within your stipulated options and budget. We will give you the freedom and peace of mind to simply enjoy your magnificent day in complete bliss.</p>
 					        <div class="image group">
 							   <div class="grid images_3_of_1">
 								<img src="images/pic.jpg" alt="">
 								</div>
+								
 									<div class="grid span_2_of_1">
-										<h4>Lorem Ipsum is simply dummy text </h4>
-											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam </p>
+										<h4>Welcome To BRIGHTON </h4>
+											<p>Our Wedding Sri Lanka is the most innovative and the leading Wedding Planner especially for Local and Destination Weddings in Sri Lanka</p>
+											<p>Traditionally, planning a wedding has always been a long process which involves a lot of time, money, effort, stress and high risks. Our Wedding Sri Lanka provides a key role in making sure that you enjoy your wedding day as you have dreamed of, and we will make sure that all things will go smoothly according to the plan.</p>
 									          <div class="more">
 								         	  <a href="#" class="btn btn-primary">more</a>
+								         	  <span class="more">
+        										<p>Our Wedding Sri Lanka is the dream of Lucky Arsacularatne that became a reality in 2007. Lucky’s vision was to establish the first one stop shop for weddings in Sri Lanka that embodied the highest standards of quality and service. Subsequently, Our Wedding Sri Lanka’s team of Wedding Planners have been incorporating the elements of artistic flair, superb organizational skills and budget management control to create the wedding visions of their clientele.</p>
+    											<p>Lucky Arsacularatne was an Overhead Controller at Brandix Group. He has worked in the BPO Industry and Forex markets and was a Quantum Programmer. He was an Event Coordinator of CHOGM (Commonwealth Heads of Government Meeting) 2013, handling the Opening Ceremony, Business Forum, Exhibition and Tour Operator Familiarizing Programs (FAM Tours). Furthermore he was an Event Consultant for the launch of Infotel Exhibition 2015.</p>
+    										</span>
+
 								         </div>
 							   		</div>
 			  				 </div>
 				      </div>
 				<div class="lsidebar1 span_1_of_a about_desc">
-				      <h2>Latest News</h2>
+				      <h2></h2>
 				     	<ul class="about-list">
                             <li>
-                                <h3>Pellentesque sed dolor. Aliquam congue fermentum nisl</h3>
-                                <p>Fusce suscipit varius mi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vestibulum molestie lacus. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. Maecenas tristique orci ac sem.</p>
+                                <h3>Our Company</h3>
+                                <p>In the field of Wedding and Event Management in Sri Lanka, Magical Moments has carved its place as a professional service provider with an inimitable focus on customization, creativity & originality. Our forte in Wedding & Event planning lies in knitting together delicate ideas and weaving them into a perfectly executed event. Be it an event for 50 or 500 guests, your ideas will become our inspiration and that is what will result in a truly momentous celebration. Our wide range of networked partnerships mean that end of the day, our clients enjoy the best benefits- and most savings. 
+Our philosophy is simple. Intricate planning - personalized touch - perfect execution - professional service. To experience it first hand,do give us a call.</p>
                             </li>
                             <li>
-                                <h3>Pellentesque sed dolor. Aliquam congue fermentum nisl</h3>
-                                <p>Fusce suscipit varius mi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vestibulum molestie lacus. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. Maecenas tristique orci ac sem.</p>
+                                <h3>Our Team</h3>
+                                <p>Lucky Arsacularatne was an Overhead Controller at Brandix Group. He has worked in the BPO Industry and Forex markets and was a Quantum Programmer. He was an Event Coordinator of CHOGM (Commonwealth Heads of Government Meeting) 2013, handling the Opening Ceremony, Business Forum, Exhibition and Tour Operator Familiarizing Programs (FAM Tours). Furthermore he was an Event Consultant for the launch of Infotel Exhibition 2015.</p>
+                            	<p>With a deep desire and passion for Wedding & Event planning since her young age, “Charm” has over the years taken that passion to another level and now enjoys more complex and larger scale event planning.With over a decade of experience in the Apparel Industry, the expertise she brings to the table far surpass client expectations. Heading the Marketing arm for several international, US & European brands of one of Sri Lanka’s largest apparel exporters, she has left behind a flourishing career to pursue this lifelong dream & passion of Wedding & Event planning.</p>
+                            	<p>With a flare for fashion , a high sense of quality & service standard along with expertise in Food & Beverage, the team at Magical Moments is well set to handle any event be it a large scale jamboree or a petite birthday gala. With apt negotiation skills to ensure you get the best deal, paired with the ability to handle any crisis situation or tight deadline; this Team holds diverse strengths & capabilities.Meet us and you will see for yourself the passion, enthusiasm, creativity and originality that have amazed many.</p>
                             </li>
                             <li>
-                                <h3>Pellentesque sed dolor. Aliquam congue fermentum nisl</h3>
-                                <p>Fusce suscipit varius mi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vestibulum molestie lacus. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. Maecenas tristique orci ac sem.</p>
-                            </li>
+                                <h3></h3>
+                                <p></p>
+                           	</li>
                         </ul>
 						</div>
 					   <div class="clear"></div>
